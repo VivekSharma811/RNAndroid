a07f69ccc04f697b0302aa5f20a2e20397dad8bd
Vivek Sharma <vs811861@gmail.com>
2019-03-08 16:50:07 +0530

    Add files via upload

---
+++ app/src/main/java/com/androidappforraxa/ToastModule.java
@@ -0,0 +1,33 @@
+package com.androidappforraxa;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public String getName() {
+
+        return "ToastExample";
+    }
+    @ReactMethod
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+}
